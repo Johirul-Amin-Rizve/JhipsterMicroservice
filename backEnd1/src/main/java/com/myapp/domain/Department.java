1db276f9f97b25bd71f9b12783575e48efbdb729
Johirul-Amin-Rizve <johirulaminrizve@gmail.com>
2022-04-21 23:04:06 +0600

    initial commit

---
+++ backEnd1/src/main/java/com/myapp/domain/Department.java
@@ -0,0 +1,137 @@
+package com.myapp.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.*;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+/**
+ * A Department.
+ */
+@Entity
+@Table(name = "department")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class Department implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "address")
+    private String address;
+
+    @OneToMany(mappedBy = "department")
+    @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+    @JsonIgnoreProperties(value = { "department" }, allowSetters = true)
+    private Set<Student> students = new HashSet<>();
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public Department id(Long id) {
+        this.setId(id);
+        return this;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Department name(String name) {
+        this.setName(name);
+        return this;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public Department address(String address) {
+        this.setAddress(address);
+        return this;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Set<Student> getStudents() {
+        return this.students;
+    }
+
+    public void setStudents(Set<Student> students) {
+        if (this.students != null) {
+            this.students.forEach(i -> i.setDepartment(null));
+        }
+        if (students != null) {
+            students.forEach(i -> i.setDepartment(this));
+        }
+        this.students = students;
+    }
+
+    public Department students(Set<Student> students) {
+        this.setStudents(students);
+        return this;
+    }
+
+    public Department addStudent(Student student) {
+        this.students.add(student);
+        student.setDepartment(this);
+        return this;
+    }
+
+    public Department removeStudent(Student student) {
+        this.students.remove(student);
+        student.setDepartment(null);
+        return this;
+    }
+
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Department)) {
+            return false;
+        }
+        return id != null && id.equals(((Department) o).id);
+    }
+
+    @Override
+    public int hashCode() {
+        // see https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
+        return getClass().hashCode();
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "Department{" +
+            "id=" + getId() +
+            ", name='" + getName() + "'" +
+            ", address='" + getAddress() + "'" +
+            "}";
+    }
+}
